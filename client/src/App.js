3aed377217ffe91aa4f3818e7a2d0bf84d8a286c
Arunava-27 <drarunkundu22@gmail.com>
2023-01-07 23:04:57 +0530

    start from login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,56 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Page from "./components/Page";
+import Register from "./components/Register";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+// root routes
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Username/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+    {
+      path: "/password",
+      element: <Password/>,
+    },
+    {
+      path: "/profile",
+      element: <Profile/>,
+    },
+    {
+      path: "/page",
+      element: <Page/>,
+    },
+    {
+      path: "/recovery",
+      element: <Recovery/>,
+    },
+    {
+      path: "/reset",
+      element: <Reset/>,
+    },
+    {
+      path: "*",
+      element: <PageNotFound/>,
+    },
+  ]
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Login App</h1>
-      </header>
-    </div>
+        <main>
+          <RouterProvider router={router}></RouterProvider>
+        </main>     
   );
 }
 
